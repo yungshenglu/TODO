bec25bfc435e81d47573b240f998fd0ebf57aea1
yungshenglu <yungshenglu1994@gmail.com>
2020-04-03 18:12:19 +0800

    Initialize TodoList component

---
+++ src/components/TodoList.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="todolist">
+        
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class TodoList extends Vue {
+    
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
