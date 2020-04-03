b2d58d9766bed09b97b6b98ea19b75818d437fda
yungshenglu <yungshenglu1994@gmail.com>
2020-04-03 18:24:05 +0800

    Implement a addTodo function for testing

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,9 +2,9 @@
     <div class="todolist">
         <h2>Simple TodoList</h2>
         <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="New TODO" aria-label="New TODO" aria-describedby="button-addon2">
+            <input type="text" class="form-control" placeholder="New TODO" aria-label="New TODO" aria-describedby="button-addon2" v-model="newTodo">
             <div class="input-group-append">
-                <button class="btn btn-outline-secondary" type="button" id="button-addon2">
+                <button class="btn btn-outline-secondary" type="button" id="button-addon2" @click="addTodo">
                     Add
                 </button>
             </div>
@@ -17,7 +17,12 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
 export default class TodoList extends Vue {
-
+    newTodo: string = '';
+    
+    addTodo() {
+        // Log for testing
+        console.log('newTodo: ', this.newTodo);
+    }
 }
 </script>
 
