ebe43965c7e18983bfc09e805cfa0d373e7066b0
davidlu <yungshenglu1994@gmail.com>
2020-07-13 10:54:14 +0800

    add: .prettierrc

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
-    <div id="app">
-        <TodoList></TodoList>
-        <span style="color: #BFBFBF">View on
-            <a-button type="link" style="padding: 0; color: #BFBFBF;" href="https://github.com/yungshenglu/Vue-TodoList/">GitHub</a-button>
-        </span>
-    </div>
+  <div id="app">
+    <TodoList></TodoList>
+    <span style="color: #BFBFBF">
+      View on
+      <a-button type="link" style="padding: 0; color: #BFBFBF;" href="https://github.com/yungshenglu/Vue-TodoList/">
+        GitHub
+      </a-button>
+    </span>
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import TodoList from './components/TodoList.vue';
+import { Component, Vue } from 'vue-property-decorator'
+import TodoList from './components/TodoList.vue'
 
 @Component({
-    components: {
-        TodoList,
-    },
+  components: {
+    TodoList
+  }
 })
 export default class App extends Vue {}
 </script>
@@ -22,11 +25,11 @@ export default class App extends Vue {}
 <style lang="scss">
 @import 'src/scss/default.scss';
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
--- src/models/TodoItem.ts
+++ src/models/TodoItem.ts
@@ -1,27 +1,27 @@
 export default class TodoItem {
-    public token: string;
-    public isDone: boolean;
-    protected content: string;
-    protected createDate: Date;
-    protected doneDate: object;
-    protected isEdit: boolean;
+  public token: string
+  public isDone: boolean
+  protected content: string
+  protected createDate: Date
+  protected doneDate: object
+  protected isEdit: boolean
 
-    constructor(content: string) {
-        this.token = this.generateToken();
-        this.content = content;
-        this.createDate = new Date();
-        this.doneDate = {};
-        this.isDone = false;
-        this.isEdit = false;
-    }
+  constructor(content: string) {
+    this.token = this.generateToken()
+    this.content = content
+    this.createDate = new Date()
+    this.doneDate = {}
+    this.isDone = false
+    this.isEdit = false
+  }
 
-    /* tslint:disable:no-bitwise */
-    private generateToken(): string {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (char) => {
-            const random = Math.random() * 16 | 0;
-            const value = (char === ('x') ? random : (random % 4 + 8));
-            return value.toString(16);
-        });
-    }
-    /* tslint:enable:no-bitwise */
+  /* tslint:disable:no-bitwise */
+  private generateToken(): string {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, char => {
+      const random = (Math.random() * 16) | 0
+      const value = char === 'x' ? random : (random % 4) + 8
+      return value.toString(16)
+    })
+  }
+  /* tslint:enable:no-bitwise */
 }
